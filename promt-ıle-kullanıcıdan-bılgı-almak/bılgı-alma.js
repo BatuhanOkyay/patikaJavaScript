13e18fcd428025f21cdfee52e3a5978259e685e7
Batuhan Okyay <talhabatuhan@hotmail.com>
2025-10-21 22:24:13 +0300

    promt kullanimi dersi eklendi

---
+++ promt-ıle-kullanıcıdan-bılgı-almak/bılgı-alma.js
@@ -0,0 +1,7 @@
+//* promt ıle kullanıcıdan bılgı almak 
+
+let fullName = prompt("Lutfen adinizi giriniz:")
+
+let greeting = document.querySelector("#greeting")
+
+greeting.innerHTML = `${greeting.innerHTML} <small style= "color:red">${fullName}</small>`
